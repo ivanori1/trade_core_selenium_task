18aaaf97a85905a16990e9483fb2bf53082d4279
ivanori1 <coric.ivan@yandex.com>
2018-08-16 13:54:10 +0200

    simple_test check title

---
+++ simple_test.py
@@ -0,0 +1,16 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class SampleTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def tradecore_test(self):
+        self.driver.get("https://demo-biq.dev.tradecore.io/#/")
+        assert self.driver.title == "TradeCore - Step 1 | Registration"
+
+    def tearDown(self):
+        self.driver.quit()
