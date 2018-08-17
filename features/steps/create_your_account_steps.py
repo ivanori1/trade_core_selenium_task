11edd8d5c3596e2b18478cfd510a519d9be42c23
ivanori1 <coric.ivan@yandex.com>
2018-08-17 11:53:13 +0200

    adopt for browser and create_your_account methods

--- features/steps/create_your_account_steps.py
+++ features/steps/create_your_account_steps.py
@@ -1,17 +1,17 @@
 from behave import given, when, then
-from selenium.webdriver.common.by import By
 
 
 @given('Go to Tradecore')
 def step_impl(context):
-    context.create_account.navigate("https://demo-biq.dev.tradecore.io/#/")
-    assert context.create_account.get_page_title() == "TradeCore - Step 1 | Registration"
+    context.browser.navigate("https://demo-biq.dev.tradecore.io/#/")
+    assert context.browser.get_page_title() == "TradeCore - Step 1 | Registration"
+
 
 @when('Click Next')
 def step_impl(context):
-    context.driver.find_element(By.ID, "button-step").click()
+    context.create_account.click_next()
 
 
 @then('This field is required error')
 def step_impl(context):
-    assert context.driver.find_element(By.CSS_SELECTOR, "[ng-message='required']").is_displayed()
+    assert context.create_account.verified_required_field_visible()
