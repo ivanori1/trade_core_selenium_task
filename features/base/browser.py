5904e5e4481b99069f3f5bfe7b7cb416bd11fa3d
ivanori1 <coric.ivan@yandex.com>
2018-08-17 11:51:22 +0200

    wrapper of selenium driver actions, copy from: ivanori1/smoke_test_webstation/blob/master/webstation/base/selenium_driver.py

--- features/base/browser.py
+++ features/base/browser.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Browser(object):
@@ -9,3 +12,46 @@ class Browser(object):
 
     def close(context):
         context.driver.close()
+
+    def navigate(self, address):
+        self.driver.get(address)
+
+    def get_page_title(self):
+        return self.driver.title
+
+    def click_element(self, *locator):
+        self.driver.find_element(*locator).click()
+
+    def get_by_type(context, locator_type):
+        if locator_type == "css":
+            return By.CSS_SELECTOR
+        elif locator_type == "xpath":
+            return By.XPATH
+        else:
+            print("Unknown selector")
+
+    def get_element(context, locator, locator_type="css"):
+        element = None
+        try:
+            by_type = context.get_by_type(locator_type)
+            element = context.driver.find_element(by_type, locator)
+        except:
+            print("element not found " + locator.upper())
+        return element
+
+    def click_on_element(context, locator, locator_type="css"):
+        try:
+            element = context.get_element(locator, locator_type)
+            element.click()
+        except:
+            print("click action not performed")
+
+    def wait_for_element_to_be_clickable(self, locator, locator_type="css"):
+        by_type = self.get_by_type(locator_type)
+        wait = WebDriverWait(self.driver, 20)
+        element = wait.until(EC.element_to_be_clickable((by_type, locator)))
+        return element
+
+    def is_visible_element(self, locator, locator_type="css"):
+        element_visible = self.get_element(locator, locator_type).is_displayed()
+        return element_visible
