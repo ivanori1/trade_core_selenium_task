cffb9c61ba940bc1fc6525425a5e96b87ea88381
ivanori1 <coric.ivan@yandex.com>
2018-08-21 17:44:13 +0200

    Questionnaire() add

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,12 @@
 from features.base.browser import Browser
 from features.pages.create_account_page import CreateAccount
+from features.pages.questionnaire_page import Questionnaire
 
 
 def before_all(context):
     context.browser = Browser()
     context.create_account = CreateAccount()
+    context.questionnaire = Questionnaire()
 
 
 def after_all(context):
