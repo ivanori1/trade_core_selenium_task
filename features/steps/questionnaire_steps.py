0dec9577b31d85b03455a6c3749fdd4f5033b003
ivanori1 <coric.ivan@yandex.com>
2018-08-22 10:04:51 +0200

    steps for Scenario: Fill Answers for Shares

--- features/steps/questionnaire_steps.py
+++ features/steps/questionnaire_steps.py
@@ -4,3 +4,11 @@ from behave import given, when, then, step
 @step('Click Finish')
 def step_impl(context):
     context.questionnaire.click_finish()
+
+@when('Select one of "{answer}" from Shares')
+def step_impl(context, answer):
+    context.questionnaire.select_from_shares(answer)
+
+@then('One of the Shares dropdown "{answer}" will be visible')
+def step_impl(context, answer):
+    context.questionnaire.verify_answer_selected_from_shares(answer)
